c8b9eca1fe612c00763ce01ae7c2fd06bcbca261
James Graca-Jones <jamesgracajones@gmail.com>
2023-06-08 20:38:42 +0100

    Follow SLAP for readability

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
-const callAPI = (n)=>{
-    const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
-    const raw = JSON.stringify({n});
-    const requestOptions = {
+function makeRequestBody(n) {
+    return JSON.stringify({ n });
+}
+
+function makeRequestHeaders() {
+    const requestHeaders = new Headers();
+    requestHeaders.append("Content-Type", "application/json");
+    return requestHeaders;
+}
+
+function makeRequestOptions(n) {
+    const headers = makeRequestHeaders();
+    const body = makeRequestBody(n);
+    return {
         method: 'POST',
-        headers: myHeaders,
-        body: raw,
-        redirect: 'follow'
+        headers,
+        body,
+        redirect: 'follow',
     };
-    fetch("https://7x9a61i6n3.execute-api.eu-west-2.amazonaws.com/production", requestOptions)
-    .then(response => response.text())
-    .then(result => alert(JSON.parse(result).body))
-    .catch(error => console.log('error', error));
+}
+
+async function getYellowstonePermutationInteger(n) {
+    const requestOptions = makeRequestOptions(n);
+    const response = await fetch(
+        "https://7x9a61i6n3.execute-api.eu-west-2.amazonaws.com/production",
+        requestOptions
+    );
+    return response.text();
+}
+
+async function displayYellowstonePermutationInteger(n) {
+    // NOTE: n is the position of the integer in the Yellowstone sequence. n == 1 corresponds 
+    // to the first integer in the sequence.
+    try {
+        const integer = await getYellowstonePermutationInteger(n);
+        alert(JSON.parse(integer).body);
+    } catch (err) {
+        console.log(err);
+    }
 }
